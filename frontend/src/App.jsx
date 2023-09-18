436c9dcc6daaedd2664435c5453e8ba3e202947d
Charlie C <zbyabqrc@pm.me>
2023-09-18 04:53:23 +0000

    Updated PhotoListItem component to show photo data on startup page, updated scss styling, added appropriate classNames to each html item

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,49 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const sampleDataForPhotoListItem = [{
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+},
+{
+  id: "2",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+},
+{
+  id: "3",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+}];
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  // /******** CHANGE photoListGrid Depending on how Data is received: *********/
+  // /**  Is the Data already in an array or not? if not then it needs to be put in an array.  **/
+  // const photoListArray = [sampleDataForPhotoListItem];
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      {sampleDataForPhotoListItem.map((photoData) => (
+        <PhotoListItem key={photoData.id} data={photoData} />
+      ))}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,25 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+
+  const { data } = props;
+
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={data.imageSource} /><br />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={data.profile} /><br />
+        <div className="photo-list__user-info">
+          {data.username}
+          <div className="photo-list__user-location">
+            {data.location.city} {data.location.country}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
