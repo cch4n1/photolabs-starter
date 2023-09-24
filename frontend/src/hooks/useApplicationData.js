f4e52af1acd3a693064c73b67939f266377ea645
Charlie C <zbyabqrc@pm.me>
2023-09-24 22:48:37 +0000

    refactored useApplicationData to use useReducer instead of useState

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,40 +1,64 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import mockPhotos from "mocks/photos";
-import mockTopics from "mocks/topics";
+
+export const ACTIONS={
+  TOGGLE_MODAL: 'TOGGLE_MODAL',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  TOGGLE_FAVOURITE: 'TOGGLE_FAVOURITE'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.TOGGLE_MODAL:
+      return {      
+        ...state,
+        isModalOpen: action.payload
+      }; /* we'll come back to add logic here soon */
+    case ACTIONS.SELECT_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.payload
+      }; /* we'll come back to add logic here soon */
+    case ACTIONS.TOGGLE_FAVOURITE:
+      const photoId = action.payload;
+      const isFavorite = state.favourites.includes(photoId);
+      const updatedFavourites = isFavorite
+        ? state.favourites.filter((favPhotoId) => favPhotoId !== photoId)
+        : [...state.favourites, photoId];
+      return { ...state, favourites: updatedFavourites };
+    default: 
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 const useApplicationData = () => {
+  
+  const initialState = {
+    isModalOpen: false,
+    selectedPhoto: null,
+    favourites: []
+  };
+ 
+ const [state, dispatch] = useReducer(reducer, initialState)
 
- // Maintain state for the modal
- const [isModalOpen, setIsModalOpen] = useState(false);
-
- // Maintain state for selected photo data: pass from home to modal
- const [selectedPhoto, setSelectedPhoto] = useState(null);
  // Function to check photo id of selected photo with database and then sets state to that photo
  const handleSelectedPhoto = (photoId) => {
    let photoSelection = mockPhotos.find((photo) => photo.id === photoId);
-   setSelectedPhoto(photoSelection);
+   dispatch({type: ACTIONS.SELECT_PHOTO, payload: photoSelection})
  };
 
- const [ favourites, setFavourites ] = useState([])
-
  const toggleFavourite = (photoId) => {
-   if (favourites.includes(photoId)) {
-     const copyOfFavourites = [ ...favourites ].filter(favPhotoId => favPhotoId !== photoId)
-     setFavourites(copyOfFavourites);
-     return;
-   }   
-
-   setFavourites(prev => [...prev, photoId]) 
- }
+  dispatch({type: ACTIONS.TOGGLE_FAVOURITE, payload: photoId})
+}
 
  return {
-  isModalOpen, 
-  setIsModalOpen,
-  selectedPhoto,
-  setSelectedPhoto,
+  isModalOpen: state.isModalOpen, 
+  setIsModalOpen: (isOpen) => dispatch({ type: ACTIONS.TOGGLE_MODAL, payload: isOpen }),
+  selectedPhoto: state.selectedPhoto,
   handleSelectedPhoto,
-  favourites,
-  setFavourites,
+  favourites: state.favourites,
   toggleFavourite
  }
 }
